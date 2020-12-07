66ab7a7e4fdf0fdafffdbf180961392c9f8b6a17
zalkar-z <zak@packet.com>
2020-12-06 21:43:34 -0500

    linking well

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,23 +3,14 @@ import PropTypes from 'prop-types'
 import { has } from 'lodash'
 
 const Video = ({ item }) => {
+  const fontWeight = has(item, 'new') ? 700 : 500;
   return (
     <div>
-      <p style={{ margin: '2px' }}>
-        <a href={item.link} target="_blank" rel="noreferrer">
-          {item.name}
+      <p style={{ margin: '2px', fontWeight: fontWeight }}>
+        <a href={has(item, 'embedYoutube') ? '/prism' : item.link} target="_blank" rel="noreferrer">
+          {has(item, 'new') ? 'NEW: ' : ''}{item.name}
         </a>
       </p>
-      {has(item, 'embedYoutube') && (
-        <iframe
-          width="100%"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/jTxfl3nJ3jA?rel=0"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      )}
     </div>
   )
 }
---
+++ src/pages/prism.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const Prism = () => {
+  return (
+    <div>
+      <iframe
+          width="100%"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/jTxfl3nJ3jA?rel=0"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+    </div>
+  )
+}
+
+export default Prism;
