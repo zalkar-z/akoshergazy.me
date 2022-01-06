61ae1229a7411017d10ab61cdecf4c1570748cb0
akylaiyms <akylaiyms@bennington.edu>
2022-01-05 20:44:48 -0800

    updated prism, dyods, and content page

--- src/pages/advanced-work.js
+++ src/pages/advanced-work.js
@@ -4,24 +4,33 @@ const AdvancedWork = () => {
   return (
     <div style={{ textAlign: 'center', padding: '80px 24%' }}>
       <div>
-        <h1>ADVANCED WORK</h1>
+        <h1>Dyscovering Your Other Digital Self (VR/Installation)</h1>
       </div>
       <div style={{ marginTop: '50px' }}>
         <iframe
             width="100%"
             height="415"
-            src="https://www.youtube-nocookie.com/embed/2P1pP670UZg?rel=0"
+            src="https://drive.google.com/file/d/1X7g8XcQczu6ovjntnyVoScR0aK0424zp/view?usp=sharing"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
       </div>
       <div style={{ marginTop: '20px', textAlign: 'left'}}>
-        <p><span style={{ fontWeight: '700' }}>ARTIST STATEMENT</span> by Ako Shergazy:</p>
-        <p id="demo" style="font-family:Arial,Helvetica,sans-serif;">
-          I couldn’t come up with the most appropriate title for my project just yet, because it is still a work-in-progress and still an amusing mystery for me. But overall, this project highlights the interdisciplinary work I have done during my time at Bennington. 
-	        This is a sound-only release of my work which will be an installation (virtual and physical) that holds a virtual world I am creating to present the compositions I wrote over the past four years. My interest in game development led me to this concept of presenting my work, the aspect of user interactivity has played a large part in the development of this idea as well. I am creating an immersive world where the audience can physically be a part of my virtual world and experience musical performances. With the entire world shifting to a remote online lifestyle I thought it would be an interesting delivery of my work to the outside world, which also aligned with my academic focus in computer science.</p>
-        <p>Allen Shawn on piano, Nathaniel Parke on Cello.</p>
+        <p><span style={{ fontWeight: '500' }}>ARTIST STATEMENT</span> by Ako Shergazy:</p>
+        <p>
+        How is data being collected? And how much of it is collected through our daily used devices? How much of it is captured through sound? 
+        In this piece, <em>Discovering Your Other Digital Self</em> I am creating an alter-ego by putting together endless pieces of information that my electronic devices are capturing through sound. 
+        Since phones are an irreplaceable part of our everyday life, I decided to use mine as a foundation for this piece. How is it like being inside your phone’s ‘head’ 
+        and recording all of these arbitrary things and making sense out of them? I have decided to create a semi-virtual reality to experience my phone’s point of view as close as I can.
+        All of the sounds here are processed field recordings from my everyday life. You can hear conversations, whispers, space ambiances, and different daily activities. 
+        The sounds are being separately fed to an algorithm that generates random combinations of four sound stems. I think computerizing the sound output through a random algorithm 
+        gives a better sense of having your phone’s POV. That creates a sense of chaos and ambiguity as the algorithm is searching through a huge pool of data while capturing 
+        more and more information. The visual patterns are the representations of how a single ‘drop’ of new data falling into the ocean of information triggers the algorithm 
+        to find infinite possibilities of different connections and meanings. You get into your phone’s head and hear yourself from its point of view. 
+
+        <em>Discovering Your Other Digital Self</em> debuted as an installation piece in Bennington College, VT in November 2021. The online VR version of the piece will be available in late 2022.
+        </p>
         <script>
         function myFunction() {
           alert(document.getElementById("demo").style.fontFamily)
--- src/pages/prism.js
+++ src/pages/prism.js
@@ -17,9 +17,10 @@ const Prism = () => {
           ></iframe>
       </div>
       <div style={{ marginTop: '20px', textAlign: 'left' }}>
-        <p><span style={{ fontWeight: '700' }}>ARTIST STATEMENT</span> by Ako Shergazy:</p>
+        <p><span style={{ fontWeight: '500' }}>ARTIST STATEMENT</span> by Ako Shergazy:</p>
         <p>I have always been inspired by sound as an independent physical phenomenon. However what fascinates me most is the importance of sound in world construction, both virtual and physical. When I first encountered the rough draft of “Prism” I saw an opportunity to challenge myself in a way I hadn’t before. In this piece, I explore how I can construct a soundscape that evokes and connects the basic senses of the human body.  The film is about the light/color spectrum and its relationship to the human sensation that I have worked on in collaboration with Emma Blaise Doty from Pratt Institute. Through this film, I am representing my understanding of basic color theory and its spatiality. In the production of sound design, I have focused on creating distinct themes for each color shown in the film. To blend out the perplexity of the moving images themes also serve as the storytellers for the viewer. Most of the material used are simple captured sounds such as singing, the jingling of keys, and brushing, which are digitally manipulated to complement the soundscape. </p>
         <p>“Prism” is designed to be watched with any type of head- or earphones to experience the full range of 3D sound. The sound design was deeply inspired by sound designers and composers Haley Shaw and Hildur Guðnadóttir</p>
+        <p>"Prism" was accepted for Best Animation in "The Psychedelic Film and Music Festival" in New York City, December 2021</p>
       </div>
     </div>
   )
